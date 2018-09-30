b87fe13b4f2bf0a9fd62632aa03e0ff563b5396e
Muhammatsorin <sorin.13122540@gamil.com>
2018-09-30 20:56:32 +0700

    commit 5.2

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,10 +1,24 @@
-import React, { Component } from 'react';
-export default class CharacterCard extends Component {     
-    render() {         
-        return (             
-            <div className = 'card'>
+import React, { Component , seq } from 'react';
+
+
+export default class CharacterCard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            active: false,
+        }
+    }
+    activate = () => {
+        if (!this.state.active) {
+            this.setState({ active: true, seq })
+        }
+    }
+    render() {
+        let className = `card ${this.state.active ? 'activeCard' : ''}`
+        return (
+            <div  className={className} onClick={this.activate}>
                 {this.props.value}
-            </div>         
-        );    
-    } 
+            </div>
+        );
+    }
 } 
