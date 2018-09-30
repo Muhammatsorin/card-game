5142630c984a07b23443042e7e4134b802f26bf3
Muhammatsorin <sorin.13122540@gamil.com>
2018-09-30 21:42:11 +0700

    commit 5.4

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,5 +1,4 @@
-import React, { Component , seq } from 'react';
-
+import React, { Component, seq } from 'react';
 
 export default class CharacterCard extends Component {
     constructor(props) {
@@ -8,15 +7,18 @@ export default class CharacterCard extends Component {
             active: false,
         }
     }
-    activate = () => {
-        if (!this.state.active) {
-            this.setState({ active: true, seq })
-        }
+    
+    activate = () => { 
+        if (!this.state.active) { 
+            this.props.activationHandler(this.props.value)         
+            this.setState({ active: true }) 
+        } 
     }
+
     render() {
         let className = `card ${this.state.active ? 'activeCard' : ''}`
         return (
-            <div  className={className} onClick={this.activate}>
+            <div className={className} onClick={this.activate}>
                 {this.props.value}
             </div>
         );
--- src/WordCard.js
+++ src/WordCard.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import CharacterCard from './CharacterCard'
 
 export default class WordCard extends Component {
-    activationHandler = c => { 
-        console.log(`${c} has been activated.`) 
+    constructor(props) {
+        super(props)
+        this.state = {
+            attempt: '',
+        }
+    }  
+        activationHandler = c => {
+            console.log(`${c} has been activated.`)
+        }
+
+        render() {
+            return (
+                <div>
+                    {Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} attempt={this.state.attempt} activationHandler={this.activationHandler} />)}
+                </div>
+            );
+        }
     }
-    render() {
-        return (
-            <div>
-                {Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} attempt={this.state.attempt} activationHandler={this.activationHandler}/> )}
-            </div>
-        );
-    }
-}
