726548445dca107d0bbdb974b4208f4366205938
Muhammatsorin <sorin.13122540@gamil.com>
2018-09-30 16:16:31 +0700

    render worldcard

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 import CharacterCard from './CharacterCard'
 import WordCard from './WordCard'
 
-const word = "Hello"; 
+const word = "Hello";
 class App extends Component {
   render() {
     return (
-    <div className="App"> 
-      { Array.from(word).map((c, i) => <WordCard value={c} key={i}/>) }          
-    </div>
+      <div className="App">
+        <WordCard value="hello" />
+      </div>
     );
   }
 }
